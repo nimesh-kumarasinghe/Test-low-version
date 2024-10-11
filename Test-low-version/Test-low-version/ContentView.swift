06236ab780fde2737fadb32a6bf1745e04c732ae
Nim <nim@Nims-Mac.local>
2024-10-11 22:39:59 +0530

    Test 01

---
+++ Test-low-version/Test-low-version/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Test-low-version
+//
+//  Created by Nim on 2024-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test-low-version/Test-low-version/Test_low_versionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_low_versionApp.swift
+//  Test-low-version
+//
+//  Created by Nim on 2024-10-11.
+//
+
+import SwiftUI
+
+@main
+struct Test_low_versionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
