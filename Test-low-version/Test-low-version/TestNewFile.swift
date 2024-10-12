3a2816009e523569ae5f7a45527f459e8df42545
Nimesh <cobsccompy4231p-005@NIBM-LAB04-PC02s-iMac.local>
2024-10-12 09:20:44 +0530

    Lab Commit

---
+++ Test-low-version/Test-low-version/TestNewFile.swift
@@ -0,0 +1,27 @@
+//
+//  TestNewFile.swift
+//  Test-low-version
+//
+//  Created by COBSCCOMPY4231P-005 on 2024-10-12.
+//
+
+import SwiftUI
+
+struct TestNewFile: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Nimesh")
+        }
+        .padding()
+    }
+}
+
+struct TestNewFile_Previews: PreviewProvider {
+    static var previews: some View {
+        TestNewFile()
+    }
+}
+
